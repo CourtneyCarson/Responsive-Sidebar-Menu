0fe99f6d4e05f3d40ce55a5eedd15b7272221fb2
Carson, CourtneyX <courtneyx.carson@intel.com>
2024-04-12 14:50:26 -0700

    when menu toggles open, the arrow buttons move up and to the right of the icon

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,14 +1,14 @@
 <template>
-    <aside :class="`${is_expanded && 'is-expanded' }`">
+    <aside :class="`${is_expanded && 'is-expanded'}`">
         <div class="logo">
             <img src="../assets/vue.svg" alt="Vue">
         </div>
 
-<div class="menu-toggle-wrap">
-    <button class="menu-toggle" @click="ToggleMenu">
-        <span class="material-icons">keyboard_double_arrow_right</span>
-    </button>
-</div>
+        <div class="menu-toggle-wrap">
+            <button class="menu-toggle" @click="ToggleMenu">
+                <span class="material-icons">keyboard_double_arrow_right</span>
+            </button>
+        </div>
 
     </aside>
 </template>
@@ -47,11 +47,27 @@ aside {
         }
     }
 
+    .menu-toggle-wrap {
+        display: flex;
+        justify-content: flex-end;
+        margin-bottom: 1rem;
+
+        position: relative;
+        top: 0;
+        transition: 0.2s ease-out;
+    }
+
+    &.is-expanded {
+        width: var(--sidebar-width);
+
+        .menu-toggle-wrap {
+            top: -3rem;
+        }
+    }
+
     @media (max-width: 768px) {
         position: fixed;
         z-index: 99;
     }
-
-
 }
 </style>
