ab110d9a250f1f4f458c0de1bb0185e4e255c82a
Carson, CourtneyX <courtneyx.carson@intel.com>
2024-04-12 14:25:01 -0700

    add in styling for sidebar so the screen is split

--- src/main.js
+++ src/main.js
@@ -1,20 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css'
-
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from './views/Home.vue'
+import router from './router'
 
 
-createApp(App).mount('#app')
 
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/', component: Home },
-        { path: '/about', component: () => import('./views/About.vue') },
-    ],
-})
+
+
+
 
 createApp(App).use(router).mount('#app')
